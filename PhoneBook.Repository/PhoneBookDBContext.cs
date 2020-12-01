e316ca06183c690d8a52094f8ba39f7739b2147a
John Kyle Leonor <johnkyle2002@gmail.com>
2020-12-01 23:20:23 +0800

    Add DBContext Repository Layer

---
+++ PhoneBook.Repository/PhoneBookDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Model;
+using System;
+
+namespace PhoneBook.Repository
+{
+    public class PhoneBookDBContext : DbContext
+    {
+        public PhoneBookDBContext(DbContextOptions<PhoneBookDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressBook> AddressBook { get; set; }
+    }
+}
