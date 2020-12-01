338fb25e1055c041884d35fae9f8b41606b2a7f9
John Kyle Leonor <johnkyle2002@gmail.com>
2020-12-02 00:11:51 +0800

    Add Address List page

---
+++ PhoneBook.Blazor/Pages/AddressList.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using PhoneBook.Interface.BusinessLogic;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Blazor.Pages
+{
+    public partial class AddressList
+    {
+        protected IEnumerable<Model.AddressBook> AddressBooks { get; set; }
+
+        [Inject] IAddressBookBusinessLogic _addressBookBusinessLogic { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            AddressBooks = await _addressBookBusinessLogic.GetAllAsync();
+        }
+    }
+}
