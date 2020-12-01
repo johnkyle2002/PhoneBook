d60e689e1bc99f136889139d08b891ac8f36bcc8
John Kyle Leonor <johnkyle2002@gmail.com>
2020-12-02 00:22:54 +0800

    Apply max 50 creating address book.

--- PhoneBook.Blazor/Pages/Index.razor.cs
+++ PhoneBook.Blazor/Pages/Index.razor.cs
@@ -11,12 +11,22 @@ namespace PhoneBook.Blazor.Pages
         protected AddressBook AddressBook { get; set; }
         protected string Error;
 
+        protected int AddedAddress = 0;
+
         async Task ValidateAddressAsync()
         {
+            if (AddedAddress >= 50)
+            {
+                Error = "You've reach the max limit please.";
+                return;
+            }
             var result = await _addressBookBusinessLogic.AddAsync(AddressBook);
 
             if (result.Succeeded)
+            {
                 AddressBook = new AddressBook();
+                AddedAddress++;
+            }
             else
             {
                 Error = result.Message;
