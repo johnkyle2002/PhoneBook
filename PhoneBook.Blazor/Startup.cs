bba495130fd7dc71853e7884711fa4bc0d4706c9
John Kyle Leonor <johnkyle2002@gmail.com>
2020-12-01 23:46:23 +0800

    remove unused file

--- PhoneBook.Blazor/Startup.cs
+++ PhoneBook.Blazor/Startup.cs
@@ -1,17 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PhoneBook.Blazor.Data;
 using PhoneBook.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PhoneBook.Blazor
 {
@@ -31,8 +24,7 @@ namespace PhoneBook.Blazor
             services.AddDbContext<PhoneBookDBContext>(o => o.UseInMemoryDatabase(databaseName:"PhoneBookDBContext"));
 
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddServerSideBlazor(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
