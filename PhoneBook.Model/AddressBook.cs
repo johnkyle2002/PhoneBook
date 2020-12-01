47595b5f74bfd2e61281c211291238ca2354e7e9
John Kyle Leonor <johnkyle2002@gmail.com>
2020-12-01 23:17:29 +0800

    Add Model layer

---
+++ PhoneBook.Model/AddressBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Model
+{
+    public class AddressBook
+    {
+        public int AddressBookID { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [MinLength(11)]
+        public string PhoneNo { get; set; }
+
+        [RegularExpression(@"^([\sA-Za-z]+)$")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
